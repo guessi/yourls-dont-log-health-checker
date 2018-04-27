cfbb8edf6dd14dfc60a3911404495a6e3f0d0113
guessi <guessi@gmail.com>
2018-04-28 00:59:08 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+YOURLS Plugin - Don't Log Health Checker
+========================================
+
+Plugin for [YOURLS](http://yourls.org) 1.7+
+
+
+Description
+-----------
+Don't log the traffic from health checker
+
+**NOTE** It is originally forked from [Don't Log Bots](https://github.com/YOURLS/dont-log-bots)
+
+
+Installation
+------------
+1. Create a folder named `dont-log-health-checker` under `./user/plugins/`
+2. Copy the files under this repository to `./user/plugins/dont-log-health-checker/`
+3. Launch Web UI, and activate the plugin via the plugins administration page
+
+
+License
+-------
+
+* [YOURLS' License](https://github.com/YOURLS/YOURLS)
+* [License](LICENSE.md)
+
+
+Contact
+-------
+* http://github.com/guessi
---
+++ plugin.php
@@ -0,0 +1,103 @@
+<?php
+/**
+Plugin Name: Don't Log Health Checker
+Plugin URI: https://github.com/guessi/yourls-dont-log-health-checker
+Description: Don't Log Health Checker
+Version: 1.0.0
+Author: guessi
+Author URI: http://github.com/guessi
+*/
+
+// No direct call
+if( !defined( 'YOURLS_ABSPATH' ) ) die();
+
+function dlhc_is_health_check() {
+    $user_agent = strtolower($_SERVER['HTTP_USER_AGENT']);
+
+    // common health checker to skip (sort alphabetically)
+    // imported from distilnetworks.com (Apr 28, 2018):
+    // - https://www.distilnetworks.com/bot-directory/category/site-monitor/
+    $keyword = array(
+        '200pleasebot',
+        'amazon route 53',
+        'amsu',
+        'cloudflare',
+        'codeguard',
+        'comodo spider',
+        'comodo ssl checker',
+        'comodo-certificate-spider',
+        'comodo-webinspector-crawler',
+        'comodospider',
+        'copperegg',
+        'crawler',
+        'crawlerprocess',
+        'crazywebcrawler',
+        'datadog',
+        'deepcrawl',
+        'downnotifier',
+        'downtimedetector',
+        'elb-healthchecker',
+        'gomezagent',
+        'gooblog',
+        'gotsitemonitor',
+        'grepnetstatbot',
+        'gtmetrix',
+        'hawkreader',
+        'hosttracker',
+        'inspingbot',
+        'internetseer',
+        'internetvista',
+        'ips-agent',
+        'linkcheck',
+        'linkstats',
+        'linktiger',
+        'loadimpactpageanalyzer',
+        'loadimpactrload',
+        'logicmonitor',
+        'mappercmd',
+        'mfibot',
+        'mon.itor.us',
+        'monitis.com',
+        'monitor',
+        'monitoring',
+        'monitority',
+        'montastic',
+        'montools',
+        'netcraft',
+        'nimbubot',
+        'nmap',
+        'nomore404',
+        'onpagebot',
+        'paessler',
+        'panopta',
+        'pingbot',
+        'pingdom',
+        'pingometer',
+        'powermapper',
+        'project25499',
+        'ptst',
+        'qualidator',
+        'siteanalyzer',
+        'sitecheck',
+        'siteimprove',
+        'sitemonitor',
+        'siteuptime',
+        'sortsitecmd',
+        'vaultpress',
+        'xenu link sleuth',
+        'zabbix'
+    );
+
+    $is_hc = (str_replace($keyword, '', $user_agent) != $user_agent);
+
+    return $is_hc;
+}
+
+// Hook register
+yourls_add_filter('shunt_update_clicks', 'dlhc_skip_if_health_checker');
+yourls_add_filter('shunt_log_redirect', 'dlhc_skip_if_health_checker');
+
+// Skip if the user-agent shows that it is a service health check
+function dlhc_skip_if_health_checker() {
+    return dlhc_is_health_check();
+}
